be528edc370d6fd66e9d7da3d55705dbe8e3c30e
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-04-18 19:06:05 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,64 @@
+    <button class="button"  style="--color: skyblue" onclick="scrollToTop()">
+        <svg class="svgIcon" viewBox="0 0 384 512">
+            <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
+        </svg>
+    </button>
+<footer id="footer">
+    <div class="get-in-touch">
+        <h3>Get In Touch</h3>
+        <span class="d-flex"><i class="bi bi-geo-alt-fill me-2"></i><p>123 Street, New York, USA</p></span>
+        <span class="d-flex"><i class="bi bi-telephone-fill me-2"></i><p>+212 629 474 030</p></span>
+        <span class="d-flex"><i class="bi bi-envelope-fill me-2"></i><p>aymenoml2002@gmail.com</p></span>   
+        <div class="social-icons">
+            <a href="#"><i class="bi bi-facebook"></i></a>
+            <a href="#"><i class="bi bi-whatsapp"></i></a>
+            <a href="#"><i class="bi bi-linkedin"></i></a>
+            <a href="#"><i class="bi bi-instagram"></i></a>
+        </div>
+    </div>
+    <div class="quick-links">
+        <h3>Quick Links</h3>
+        <ul>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Home</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">About Us</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Our Services</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Our Projects</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="popular-links">
+        <h3>Popular Links</h3>
+        <ul>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Home</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">About Us</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Our Services</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Our Projects</a></li>
+            <li><i class="fa fa-angle-right me-2"></i><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="newsletter">
+        <h3>Newsletter</h3>
+        <input type="text" placeholder="Your Name">
+        <input type="email" placeholder="Your Email">
+        <input type="submit" value="Submit Now">
+    </div>
+</footer>
+<script src="assets/js/jquery-1.11.0.min.js"></script>
+   <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+   <script src="assets/js/bootstrap.bundle.min.js"></script>
+   <script src="assets/js/script.js"></script>
+   <script src="assets/js/custom.js"></script>
+   <script src="assets/js/slick.min.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
